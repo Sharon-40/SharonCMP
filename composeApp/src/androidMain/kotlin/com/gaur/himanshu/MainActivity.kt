0d429a01a3df3f450eaa2691e5cd4212e3c9cbf4
jagadishan <jagadishlakkur@gmail.com>
2024-01-20 12:20:49 +0530

    first commit

---
+++ composeApp/src/androidMain/kotlin/com/gaur/himanshu/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.gaur.himanshu
+
+import App
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import initKoin
+import org.koin.dsl.module
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
