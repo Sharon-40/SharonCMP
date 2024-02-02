86b7d2fcc7c81eff08ba943da51a88d7f45ec4ee
jagadishan <jagadishlakkur@gmail.com>
2024-02-02 12:01:31 +0530

    desktop userDetails hardcoded

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,22 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import data.preferences.LocalSharedStorage
+import org.koin.compose.koinInject
+import org.koin.java.KoinJavaComponent.inject
 
 fun main() = application {
 
     initKoin()
 
-    Window(onCloseRequest = ::exitApplication, title = "WareHouse") {
+    val localSharedStorage: LocalSharedStorage = koinInject()
+
+    localSharedStorage.saveUserId("JN")
+    localSharedStorage.saveUserName("JN")
+    localSharedStorage.savePlant("AW02")
+    localSharedStorage.saveWareHouse("OS00")
+    localSharedStorage.savePrinter("LP01")
+
+    Window(onCloseRequest = ::exitApplication, title = StringResources.AppName) {
         App()
     }
 }
