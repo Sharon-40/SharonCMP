efb2b33c42e1ca40fb640ceb74dda3f6059b20fb
jagadishan <jagadishlakkur@gmail.com>
2024-02-03 17:31:45 +0530

    iOS alert Dialog added

--- composeApp/src/iosMain/kotlin/data/Utils.kt
+++ composeApp/src/iosMain/kotlin/data/Utils.kt
@@ -1,8 +1,33 @@
 package data
 
+import platform.UIKit.UIAlertAction
+import platform.UIKit.UIAlertActionStyleDefault
+import platform.UIKit.UIAlertController
+import platform.UIKit.UIAlertControllerStyleAlert
+import platform.UIKit.UIApplication
+
 actual class Utils actual constructor(context: Any?) {
 
     actual fun makeToast(message:String){
-        print(message)
+
+        val alertController = UIAlertController.alertControllerWithTitle(
+            title = null,
+            message = message,
+            preferredStyle = UIAlertControllerStyleAlert
+        )
+
+        val action = UIAlertAction.actionWithTitle(
+            title = "OK",
+            style = UIAlertActionStyleDefault,
+            handler = {
+                // Handle button tap if needed
+                alertController.dismissViewControllerAnimated(true, null)
+            }
+        )
+        alertController.addAction(action)
+
+
+        val presentingViewController = UIApplication.sharedApplication.keyWindow?.rootViewController
+        presentingViewController?.presentViewController(alertController, animated = true, completion = {})
     }
 }
